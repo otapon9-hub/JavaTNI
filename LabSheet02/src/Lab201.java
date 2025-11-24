f0e8e9ef24bcfbdfc3d44a3aac6425aa99fd9cf9
otapon9-hub <otapon9@gmail.com>
2025-11-24 17:00:57 +0700

    Tanapon 2513110458

---
+++ LabSheet02/src/Lab201.java
@@ -0,0 +1,13 @@
+public class Lab201 {
+    public static void main(String[] args) {
+        int number1 = 10;
+        int number2 = 8;
+
+
+        System.out.println("Summation = " + (number1 + number2));
+        System.out.println("Subtraction = " + (number1 - number2));
+        System.out.println("Multiplication = " + (number1* number2));
+        System.out.println("Division = " + ((double)number1 / number2));
+        System.out.println("Modulus = " + (number1 % number2));
+    }
+}
